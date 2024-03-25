d08adaf996d75aa30449418e9f51a4992cb2d094
Eduardo9345 <jose.evangelista@academico.ifpb.edu.br>
2024-03-24 22:20:53 -0300

    first commit

---
+++ src/main/java/com/eventoapp/controller/EventoController.java
@@ -0,0 +1,40 @@
+package com.eventoapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.eventoapp.models.Evento;
+import com.eventoapp.repository.EventoRepository;
+
+@RestController
+public class EventoController {
+	
+	@Autowired
+	private EventoRepository er;
+	
+	@GetMapping("/cadastrarEvento")
+	public ModelAndView form() {
+		return new ModelAndView("evento/formEvento");
+	}
+	
+	@PostMapping("/cadastrarEvento")
+	public ModelAndView form(Evento evento) {
+		
+		er.save(evento);
+		
+		return new ModelAndView("redirect:/cadastrarEvento");
+	}
+	
+	@GetMapping("/eventos")
+	public ModelAndView listaEventos() {
+		ModelAndView mv = new ModelAndView("index");
+		Iterable<Evento> eventos = er.findAll();
+		mv.addObject("eventos", eventos);
+		return mv;
+	}
+	
+}
---
+++ src/main/java/com/eventoapp/models/Evento.java
@@ -0,0 +1,67 @@
+package com.eventoapp.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="eventos")
+public class Evento implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column
+	private String nome;
+	
+	@Column
+	private String local;
+	
+	@Column
+	private String data;
+	
+	@Column
+	private String horario;
+	
+
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLocal() {
+		return local;
+	}
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getHorario() {
+		return horario;
+	}
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+	
+	
+}
