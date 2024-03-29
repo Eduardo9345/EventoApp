c5be5c64a97fcd5bb6028731302fa80a5d21dc65
Eduardo9345 <jose.evangelista@academico.ifpb.edu.br>
2024-03-28 22:21:10 -0300

    Funcionalidade de Detalhamento de Eventos

--- src/main/java/com/eventoapp/controller/EventoController.java
+++ src/main/java/com/eventoapp/controller/EventoController.java
@@ -2,6 +2,7 @@ package com.eventoapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +35,15 @@ public class EventoController {
 		ModelAndView mv = new ModelAndView("index");
 		Iterable<Evento> eventos = er.findAll();
 		mv.addObject("eventos", eventos);
+		return mv;
+	}
+	
+	
+	@GetMapping("/{codigo}")
+	public ModelAndView detalhesEvento(@PathVariable long codigo) {
+		Evento ev = er.findById(codigo);
+		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+		mv.addObject("evento", ev);
 		return mv;
 	}
 	
--- src/main/java/com/eventoapp/repository/EventoRepository.java
+++ src/main/java/com/eventoapp/repository/EventoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.eventoapp.models.Evento;
 
 public interface EventoRepository extends CrudRepository<Evento, Long>{
-
+	Evento findById(long id);
 }
